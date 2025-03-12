1d6c964a609d57c04d44b2d816c46e8fc8624397
Christian Rawley <christianrawley0@gmail.com>
2025-03-12 16:57:58 -0500

    idk what im doing

--- routes/getSubjects.js
+++ routes/getSubjects.js
@@ -8,27 +8,25 @@ const httpsAgent = new https.Agent({ rejectUnauthorized: false });
 
 router.get('/getSubjects', async (req, res) => {
     try {
+        // Launch puppeteer browser
         const browser = await puppeteer.launch({ headless: true });
         const page = await browser.newPage();
         await page.goto(URL, { waitUntil: 'networkidle2' });
         await page.waitForSelector('#pvExplorationHost');
-
         const subjects = await page.evaluate(() => {
             const subjectElements = document.querySelectorAll(
-                '#pvExplorationHost > div > div > exploration > div > explore-canvas > div > div.canvasFlexBox > div > div.displayArea.disableAnimations.fitToPage > div.visualContainerHost.visualContainerOutOfFocus > visual-container-repeat > visual-container:nth-child(5) > transform > div > div.visualContent > div > div > visual-modern > div > div > div.slicer-content-wrapper > div > div.slicerBody > div > div.scrollbar-inner.scroll-content.scroll-scrolly_visible > div > div > div'
+                '#pvExplorationHost > div > div > exploration > div > explore-canvas > div > div.canvasFlexBox > div > div.displayArea.disableAnimations.actualSizeAlignLeft.actualSizeAlignMiddle.actualSizeOrigin > div.visualContainerHost.visualContainerOutOfFocus > visual-container-repeat > visual-container:nth-child(5) > transform > div > div.visualContent > div > div > visual-modern > div > div > div.slicer-content-wrapper > div > div.slicerBody > div > div.scrollbar-inner.scroll-content.scroll-scrolly_visible > div > div > div > div > span'
             );
-
             const subjects = [];
             subjectElements.forEach((element) => {
-                const subjectText = element.querySelector('div > span') ? element.querySelector('div > span').textContent.trim() : '';
-                if (subjectText) {
-                    subjects.push(subjectText);
+                const subject = element.textContent.trim();
+                if (subject) {
+                    subjects.push(subject);
                 }
             });
 
             return subjects;
         });
-
         await browser.close();
         res.json(subjects);
     } catch (error) {
