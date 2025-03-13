cfd5dac4a45e6c367a06c6df6b8ae2ea95000f61
Christian Rawley <christianrawley0@gmail.com>
2025-03-12 20:06:18 -0500

    prod test

--- routes/getSubjects.js
+++ routes/getSubjects.js
@@ -10,11 +10,21 @@ router.get('/getSubjects', async (req, res) => {
         browser = await puppeteer.launch({ headless: true });
         const page = await browser.newPage();
         await page.goto(URL, { waitUntil: 'networkidle2' });
-        await page.waitForXPath('/html/body/div[1]/report-embed//span');
-        const subjects = await page.evaluate(() => {
-            const spans = Array.from(document.querySelectorAll('span'));
-            return spans.map(span => span.innerText.trim()).filter(text => text.length > 0);
-        });
+        const xpaths = [
+            "/html/body/div[1]/report-embed/div/div/div[1]/div/div/div/exploration-container/div/div/docking-container/div/div/div/div/exploration-host/div/div/exploration/div/explore-canvas/div/div[2]/div/div[2]/div[2]/visual-container-repeat/visual-container[5]/transform/div/div[3]/div/div/visual-modern/div/div/div[2]/div/div[2]/div/div[1]/div/div/div[1]/div/span",
+            "/html/body/div[1]/report-embed/div/div/div[1]/div/div/div/exploration-container/div/div/docking-container/div/div/div/div/exploration-host/div/div/exploration/div/explore-canvas/div/div[2]/div/div[2]/div[2]/visual-container-repeat/visual-container[5]/transform/div/div[3]/div/div/visual-modern/div/div/div[2]/div/div[2]/div/div[1]/div/div/div[2]/div/span",
+            "/html/body/div[1]/report-embed/div/div/div[1]/div/div/div/exploration-container/div/div/docking-container/div/div/div/div/exploration-host/div/div/exploration/div/explore-canvas/div/div[2]/div/div[2]/div[2]/visual-container-repeat/visual-container[5]/transform/div/div[3]/div/div/visual-modern/div/div/div[2]/div/div[2]/div/div[1]/div/div/div[4]/div/span"
+        ];
+
+        const subjects = [];
+
+        for (let xpath of xpaths) {
+            const [element] = await page.$x(xpath);
+            if (element) {
+                const text = await page.evaluate(el => el.innerText.trim(), element);
+                subjects.push(text);
+            }
+        }
 
         console.log('Subjects:', subjects);
         res.json({ subjects });
