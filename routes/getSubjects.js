426cd622be1d3283b3dc2d647f637c57b439f74e
Christian Rawley <christianrawley0@gmail.com>
2025-03-12 21:33:58 -0500

    cont scroll

--- routes/getSubjects.js
+++ routes/getSubjects.js
@@ -6,37 +6,44 @@ const URL = 'https://app.powerbi.com/view?r=eyJrIjoiYTMzNmY3ZTgtZDdkNy00M2E2LWFi
 const SUBJECT_SELECTOR = '#pvExplorationHost div.slicer-content-wrapper div.slicerBody div div.scrollbar-inner div div div div span';
 const SCROLL_REGION_SELECTOR = '#pvExplorationHost > div > div > exploration > div > explore-canvas > div > div.canvasFlexBox > div > div.displayArea.disableAnimations.fitToPage > div.visualContainerHost.visualContainerOutOfFocus > visual-container-repeat > visual-container:nth-child(5) > transform > div > div.visualContent > div > div > visual-modern > div > div > div.slicer-content-wrapper > div > div.slicerBody > div > div.scrollbar-inner.scroll-content.scroll-scrolly_visible';
 
+async function scrollAndCollect(page) {
+    let allSubjects = [];
+    let previousLength = 0;
 
-async function scrollToBottom(page) {
-    await page.evaluate(async (scrollRegionSelector) => {
-        const scrollRegion = document.querySelector(scrollRegionSelector);
-        if (scrollRegion) {
-            let lastScrollTop = scrollRegion.scrollTop;
-            while (true) {
+    while (true) {
+            await page.evaluate((scrollRegionSelector) => {
+            const scrollRegion = document.querySelector(scrollRegionSelector);
+            if (scrollRegion) {
                 scrollRegion.scrollTop = scrollRegion.scrollHeight;
-                await new Promise(resolve => setTimeout(resolve, 1000));
-                if (scrollRegion.scrollTop === lastScrollTop) {
-                    break;
-                }
-                lastScrollTop = scrollRegion.scrollTop;
             }
+        }, SCROLL_REGION_SELECTOR);
+
+        await page.waitForTimeout(1000);
+
+        const subjects = await page.$$eval(SUBJECT_SELECTOR, spans => {
+            return spans.map(span => span.innerText.trim())
+        });
+
+        if (subjects.length > previousLength) {
+            allSubjects = [...new Set([...allSubjects, ...subjects])];
+            previousLength = subjects.length;
+        } else {
+            break;
         }
-    }, SCROLL_REGION_SELECTOR);
+    }
+    return allSubjects;
 }
 
 router.get('/getSubjects', async (req, res) => {
     try {
-        const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox']});
+        const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
         const page = await browser.newPage();
-
-        await page.goto(URL, { waitUntil: 'networkidle2'});
-        await page.waitForSelector(SUBJECT_SELECTOR);
-        await scrollToBottom(page);
-
-        const subjects = await page.$$eval(SUBJECT_SELECTOR, spans => 
-            spans.map(span => span.innerText.trim())
-        );
         
+        await page.goto(URL, { waitUntil: 'networkidle2' });
+        await page.waitForSelector(SUBJECT_SELECTOR);
+
+        const subjects = await scrollAndCollect(page);
+
         await browser.close();
         res.json({ subjects });
     } catch (error) {
