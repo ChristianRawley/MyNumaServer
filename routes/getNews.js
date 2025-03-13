8c82fa1cf143655d813abe676cc994ca1a2b8782
Christian Rawley <christianrawley0@gmail.com>
2025-03-13 16:36:41 -0500

    news is better

--- routes/getNews.js
+++ routes/getNews.js
@@ -4,27 +4,27 @@ const cheerio = require('cheerio');
 const https = require('https');
 
 const router = express.Router();
-const API_URL = 'https://uafs.edu/news/stories.php?categories=News&archives=';
 const httpsAgent = new https.Agent({ rejectUnauthorized: false });
 
-router.get('/getNews', async (req, res) => {
+router.get('/getNews/:page?', async (req, res) => {
     try {
-        const { data } = await axios.get(API_URL, { httpsAgent });
+        const URL = `https://uafs.edu/news/stories.php?categories=News&archives=${req.params.page ? `&page=${req.params.page}` : ''}`;
+        const { data } = await axios.get(URL, { httpsAgent });
         const $ = cheerio.load(data);
 
         const news = [];
         $('div.section > div.container > div.row > div.col-lg-9.category-news-list > div.row.card.plain').each((_, el) => {
-            const image = $(el).find('div.col-lg-4 > div.image > img').attr('src');
             const title = $(el).find('div.col-lg-8 > div.card-body > h3 > a').text().trim();
-            const link = $(el).find('div.col-lg-8 > div.card-body > h3 > a').attr('href');
             const description = $(el).find('div.col-lg-8 > div.card-body > p.description').text().trim();
+            const image = $(el).find('div.col-lg-4 > div.image > img').attr('src');
+            const link = $(el).find('div.col-lg-8 > div.card-body > h3 > a').attr('href');
 
             if (image && title && link && description) {
                 news.push({
-                    image: image.startsWith('http') ? image : `https://uafs.edu${image}`,
                     title,
-                    link: link.startsWith('http') ? link : `https://uafs.edu${link}`,
                     description,
+                    image: image.startsWith('http') ? image : `https://uafs.edu${image}`,
+                    link: link.startsWith('http') ? link : `https://uafs.edu${link}`,
                 });
             }
         });
